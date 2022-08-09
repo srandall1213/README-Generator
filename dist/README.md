7271be12d53e671421da322af76b6e2e9bd22a06
Randie <srandall1213@gmail.com>
2022-08-09 09:23:45 -0600

    renderLicenseSection works

---
+++ dist/README.md
@@ -0,0 +1,34 @@
+# test
+
+## License Badge
+![GitHub license](url to that license) 
+
+## Description
+test
+
+## Table of Contents
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [License](#License)
+  - [Questions](#Questions)
+
+## Installation
+test
+
+## Usage
+test
+
+## Contributing
+test
+
+## Tests
+test
+
+## License
+The project is licensed under the MIT license.
+
+## Questions
+Please email me at test if you have any questions about the repo. You can find more of my work at [test](https://github.com/test/).
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,7 +8,13 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "") {
+    return ""
+  } else {
+    return `The project is licensed under the ${license} license.`
+  }
+} 
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -41,7 +47,7 @@ ${data.contributing}
 ${data.tests}
 
 ## License
-The project is licensed under the ${data.license} license. 
+${renderLicenseSection(data.license)}
 
 ## Questions
 Please email me at ${data.email} if you have any questions about the repo. You can find more of my work at [${data.username}](https://github.com/${data.username}/).
